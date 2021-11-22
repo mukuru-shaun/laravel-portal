ad57bb80ffb57f1b516ed7bcbe6eb53d1f902e6b
Shaun Alberts <shaun@mukuru.com>
2021-11-22 21:03:54 +0200

    Refactor user stuff to use service

--- app/Http/Controllers/OAuthController.php
+++ app/Http/Controllers/OAuthController.php
@@ -2,35 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Services\User\UserService;
 use Auth;
-use Illuminate\Http\Request;
 use Socialite;
 
 class OAuthController extends Controller
 {
+    public function __construct(
+        protected UserService $userService
+    ) {}
     public function redirectToProvider()
     {
         return Socialite::driver('azure')
             ->setScopes(['User.Read.All'])
             ->redirect();
-
     }
 
     public function providerCallback()
     {
         $user = Socialite::driver('azure')->user();
 
-        $authUser = User::where('email', '=', $user->getEmail())->first();
-
-        if (! $authUser) {
-            $authUser = User::create([
-                'name' => $user->getName(),
-                'email' => $user->getEmail(),
-                'provider' => 'azure',
-                'provider_id' => $user->getId(),
-            ]);
-        }
+        $authUser = $this->userService->findUserByEmail($user->getEmail())
+            ?? $this->userService->createUser($user->getName(), $user->getEmail(), 'azure', $user->getId());
 
         // Instead of storing the token in the db for now we will just keep it in the session
         session(['azureToken' => $user->token]);
--- app/Services/User/UserService.php
+++ app/Services/User/UserService.php
@@ -3,20 +3,50 @@
 namespace App\Services\User;
 
 use App\Models\User;
-use Illuminate\Validation\Factory;
-use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
 use phpseclib3\Crypt\Common\PublicKey;
 use phpseclib3\Crypt\PublicKeyLoader;
 use App\Services\ValidationException;
 
 class UserService
 {
-    public function __construct(
-        private Factory $validationFactory
-    ) {}
+    public function findUserByEmail(string $email): ?User
+    {
+        return $authUser = User::where('email', '=', $email)->first();
+    }
+
+    public function createUser(
+        string $name,
+        string $email,
+        string $provider,
+        string $providerId,
+    ) {
+        $validation = Validator::make([
+            'name' => $name,
+            'email' => $email,
+            'provider' => $provider,
+            'providerId' => $providerId,
+        ], [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'provider' => ['required', 'string'],
+            'providerId' => ['required', 'string'],
+        ]);
+
+        if ($validation->fails()) {
+            throw (new ValidationException())->setErrors($validation->errors());
+        }
+
+        return User::create([
+            'name' => $name,
+            'email' => $email,
+            'provider' => $provider,
+            'provider_id' => $providerId,
+        ]);
+    }
 
     public function save(array $data) {
-        $validation = $this->validationFactory->make($data, [
+        $validation = Validator::make($data, [
             'id' => [
                 'exists:users,id'
             ],
