d8d01957c44cbeb02d05f8fa7a5cf778bebeccb7
Shaun Alberts <shaun@mukuru.com>
2021-11-22 20:26:24 +0200

    Install laravel debugbar

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
@@ -230,6 +232,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Debugbar' => Barryvdh\Debugbar\Facade::class
     ],
 
 ];
